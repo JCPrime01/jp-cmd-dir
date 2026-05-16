a3c95f5d94f80387874d767d06f96268e35bd920
JCPrime01 <vanzokiller@gmail.com>
2026-05-16 09:47:43 -0300

    Add more WhatsApp community links to index.js

--- api/index.js
+++ api/index.js
@@ -11,10 +11,21 @@ export default async function handler(req, res) {
     return;
   }
   const links = [
-    "https://chat.whatsapp.com/EJm8Xu09IQC8g2pMeiehro",
-    "https://chat.whatsapp.com/FqEu31mnpWg1sb8p1obh68",
-    "https://chat.whatsapp.com/IZNGcTD7uuq2M1cfB7FLlR",
-    "https://chat.whatsapp.com/EzadtmN5alqFfcBHOgK8Ji",
+    "https://chat.whatsapp.com/EJm8Xu09IQC8g2pMeiehro", // comunidade 1
+    "https://chat.whatsapp.com/FqEu31mnpWg1sb8p1obh68", // comunidade 2
+    "https://chat.whatsapp.com/IZNGcTD7uuq2M1cfB7FLlR", // comunidade 3
+    "https://chat.whatsapp.com/EzadtmN5alqFfcBHOgK8Ji",  // comunidade 4
+    "https://chat.whatsapp.com/EoLhmmeiQpGAHQ1yU9CW7A", // comunidade 104
+    "https://chat.whatsapp.com/JhDM8UdeHQg8pxIm0ywl80", // comunidade 105
+    "https://chat.whatsapp.com/EafpD39fayIIyRjOCC6Pfk",  // comunidade 106
+    "https://chat.whatsapp.com/BaFz3FnnxTb3Dbd9c3r7Wm", // comunidade 107
+    "https://chat.whatsapp.com/KSJPKfTD7tvKAL0SrorOZp", // comunidade 131
+    "https://chat.whatsapp.com/HdxZGcDeTd0KKnLrvjLjHS", // comunidade 109
+    "https://chat.whatsapp.com/EAogaedZmUQ5X7C9uFh6V2",  // comunidade 713
+    "https://chat.whatsapp.com/HX8Eh87WZ7h6cwMqnsNQmm", // comunidade 714
+    "https://chat.whatsapp.com/J8jqLjlVCwZAJIqNWR7Yr0",  // comunidade 715
+    "https://chat.whatsapp.com/FQH4sVDKle6DRxM8qHTHV5",  // comunidade 716
+    "https://chat.whatsapp.com/EklvDccWc2pK5zgXvKBTtz",  // comunidade 718
   ];
   const link = links[Math.floor(Math.random() * links.length)];
   res.writeHead(302, { Location: link });
